8048bb9033626d44134d8f81ead6f21c8b66425e
cy <15652338313@163.com>
2021-12-04 17:46:44 +0800

    menu

---
+++ CodeShare.API/Controllers/MenuController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeShare.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenuController : BaseController
+    {
+        public MenuController()
+        {
+
+        }
+
+        /// <summary>
+        /// 创建菜单--自定义
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost("createMenu")]
+        public IActionResult CreateMenu() 
+        {
+            return Ok("");
+        }
+    }
+}
